799d056f6a448845cafcde2336df56e50845d423
Chandra Prakash <chandraprakash8340@gmail.com>
2024-01-22 17:46:46 +0530

    SearchMovie Done

--- force-app/main/default/lwc/movieLwc/movieLwc.js
+++ force-app/main/default/lwc/movieLwc/movieLwc.js
@@ -1,3 +1,39 @@
-import { LightningElement } from 'lwc';
+// movieAppComponent.js
+import { LightningElement, wire } from 'lwc';
+import getMovieData from '@salesforce/apex/MovieAppController.getMovieData';
+import getSearchMovie from '@salesforce/apex/MovieAppController.getSearchMovie';
 
-export default class MovieLwc extends LightningElement {}
+export default class MovieAppComponent extends LightningElement {
+
+   showAllMovies = false;
+   showSearchedMovie = false;
+   seachedMovieData;
+   moviesData ;
+    @wire(getMovieData)
+   getData({data, error}) {
+      if(data) {
+         this.moviesData = data;
+         this.showAllMovies = true;
+      }
+      else {
+         console.log(error);
+      }
+    }
+
+    searchMovie(event) {
+      const selectMovieId = event.detail.recordId;
+      console.log(selectMovieId);
+      getSearchMovie({ recordId : selectMovieId})
+         .then(result => {
+            this.seachedMovieData= result;
+            console.log(result);
+            this.showAllMovies = false;
+            this.showSearchedMovie = true;
+         })
+         .catch(error =>{
+            console.error('Error retrieving selected movie:', error);
+         })
+   
+   }
+    
+}
